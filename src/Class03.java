7ec3b009d5a5f692c7a814c21ee8f764a7031e65
Adita78 <captari.ad@gmail.com>
2022-09-27 20:07:50 -0400

    third commit

--- src/Class03.java
+++ src/Class03.java
@@ -4,6 +4,7 @@ public class Class03 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Change in my project");
+		System.out.println("How are you? new version");
 	}
 
 }
